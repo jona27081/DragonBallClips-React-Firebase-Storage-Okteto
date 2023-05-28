45dd1babd40c87f8ce9efabedf2c37a65336625d
Jonathan Ojeda Rosas <zs20006736@estudiantes.uv.mx>
2023-05-28 01:29:12 -0600

    update reactions

--- src/components/reacciones.component.js
+++ src/components/reacciones.component.js
@@ -2,108 +2,54 @@ import React from "react";
 import '../styles/clip.css'
 import KafkaService from "../services/kafka.service";
 
-function saveLike(e, status) {
+function reaction(e, status) {
   let data = {
     id: 0,
     status: status
   };
 
   console.log(JSON.stringify(data));
-  KafkaService.reactionPush("Like");
+  KafkaService.reactionPush(status);
   e.preventDefault();
 }
 
-function saveLove(e, status) {
-  let data = {
-    id: 0,
-    status: status
-  };
-
-  console.log(JSON.stringify(data));
-  KafkaService.reactionPush("Love");
-  e.preventDefault();
-}
-
-function saveHaHa(e, status) {
-  let data = {
-    id: 0,
-    status: status
-  };
-
-  console.log(JSON.stringify(data));
-  KafkaService.reactionPush("Haha");
-  e.preventDefault();
-}
-
-function saveWow(e, status) {
-  let data = {
-    id: 0,
-    status: status
-  };
-
-  console.log(JSON.stringify(data));
-  KafkaService.reactionPush("Wow");
-  e.preventDefault();
-}
-
-function saveSad(e, status) {
-  let data = {
-    id: 0,
-    status: status
-  };
-
-  console.log(JSON.stringify(data));
-  KafkaService.reactionPush("Sad");
-  e.preventDefault();
-}
-
-function saveAngry(e, status) {
-  let data = {
-    id: 0,
-    status: status
-  };
-
-  console.log(JSON.stringify(data));
-  KafkaService.reactionPush("Angry");
-  e.preventDefault();
-}
 
 function ReactionsComponent() {
   return (
     <div class="reactions">
       <div className="reaction reaction-like" onClick={(e) => {
         e.preventDefault();
-        saveLike(e, 1);
+        reaction(e, "like");
       }}>
       </div>
       
       <div className="reaction reaction-love" onClick={(e) => {
         e.preventDefault();
-        saveLove(e, 1);
+        reaction(e, "love");
       }}>
       </div>
 
       <div className="reaction reaction-haha" onClick={(e) => {
         e.preventDefault();
-        saveHaHa(e, 1);
+        reaction(e, "haha");
       }}>
       </div>
 
       <div className="reaction reaction-wow" onClick={(e) => {
         e.preventDefault();
-        saveWow(e, 1);
+        reaction(e, "wow");
       }}>
       </div>
 
       <div className="reaction reaction-sad" onClick={(e) => {
         e.preventDefault();
-        saveSad(e, 1);
+        reaction(e, "sad");
       }}>
       </div>
 
-      <div className="reaction reaction-sad" onClick={(e) => {
+      <div className="reaction reaction-angry" onClick={(e) => {
         e.preventDefault();
-        saveAngry(e, 1);
+        reaction(e, "angry");
       }}>
       </div>
     </div>
